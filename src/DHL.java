1541e0569804afacfd96998ba450639e2ba137f2
Beckhan <Bekhan97@gmail.com>
2025-02-12 00:43:17 +0100

    Add class DHL
    - implement method sendMail();

---
+++ src/DHL.java
@@ -0,0 +1,6 @@
+public class DHL implements MailDeliveryService {
+    @Override
+    public void sendMail() {
+        System.out.println("Put the letter in an envelope, put a stamp on it, mail it.");
+    }
+}
