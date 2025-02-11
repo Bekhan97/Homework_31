38fdb0420aeef422cccfbb7fe284e602fc050ea8
Beckhan <Bekhan97@gmail.com>
2025-02-12 00:49:43 +0100

    Add class Pigeon
    - implement method sendMail();

---
+++ src/Pigeon.java
@@ -0,0 +1,6 @@
+public class Pigeon implements MailDeliveryService {
+    @Override
+    public void sendMail() {
+        System.out.println("A pigeon?! Are you being serious?! In twenty first century?! You are crazy dude!");
+    }
+}
